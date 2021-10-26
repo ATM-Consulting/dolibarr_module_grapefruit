a525c43881b21e1de13c082f10bfd21732d9ca89
jpb <jean-pascal.boudet@atm-consulting>
2021-10-26 11:38:00 +0200

    fix token + add changelog to module

---
+++ ChangeLog.md
@@ -0,0 +1,8 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+
+## 1.5.1
+
+- FIX : TOKEN RENEWAL  *25/10/2021*  1.5.1
+
--- script/interface.php
+++ script/interface.php
@@ -1,8 +1,9 @@
 <?php
+if (!defined('NOTOKENRENEWAL')) define('NOTOKENRENEWAL', 1); // Disables token renewal
 	require '../config.php';
-	
+
 	dol_include_once('/grapefruit/lib/grapefruit.lib.php');
-	
+
 	$langs->load("agenda");
 	$langs->load("other");
 
@@ -16,51 +17,51 @@
 		case 'fullcalandar_tasks':
 			$projectid = GETPOST('projectid');
 			return grapefruitGetTasksForProject('fk_task', -1, 0, $projectid);
-			
+
 			break;
-			
+
 		default:
 			break;
 	}
-	
+
 	switch ($set) {
 		case 'defaultTVA':
-			
+
 			_updateTVA(GETPOST('element'), GETPOST('element_id'), GETPOST('default_tva'));
-			
+
 			break;
-		
+
 		case 'defaultProgress':
-			
+
 			_updateProgress(GETPOST('element_id'), GETPOST('default_progress'));
-			
+
 			break;
 		default:
 			break;
 	}
 
 
-	
+
 function _updateTVA($element, $element_id, $default_tva)
 {
 	global $langs,$conf,$db;
-	
+
 	if (!empty($conf->global->GRAPEFRUIT_DEFAULT_TVA_ON_DOCUMENT_CLIENT_ENABLED) && $default_tva != '' && in_array($element, array('propal', 'commande', 'facture')))
 	{
 		dol_include_once('/comm/propal/class/propal.class.php');
 		dol_include_once('/commande/class/commande.class.php');
 		dol_include_once('/compta/facture/class/facture.class.php');
 		if (!empty($conf->subtotal->enabled)) dol_include_once('/subtotal/class/subtotal.class.php');
-		
+
 		$langs->load('grapefruit@grapefruit');
 
 		$classname = ucfirst($element);
-		
+
 		$object = new $classname($db);
 		$object->fetch($element_id);
-		
+
 		if ($object->statut != $classname::STATUS_DRAFT) return 0;
-		
+
 		$object->db->begin();
 		$res = 1;
 		foreach ($object->lines as &$l)
@@ -97,24 +98,24 @@ function _updateTVA($element, $element_id, $default_tva)
 			setEventMessage($langs->trans('grapefruit_success_tva_updateline'));
 			return 1;
 		}
-		
+
 	}
 }
 
 function _updateProgress($element_id, $default_progress)
 {
 	global $langs,$conf,$db;
-	
+
 	if (!empty($conf->global->GRAPEFRUIT_SITUATION_INVOICE_DEFAULT_PROGRESS) && is_numeric($default_progress))
 	{
 		dol_include_once('/compta/facture/class/facture.class.php');
 		if (!empty($conf->subtotal->enabled)) dol_include_once('/subtotal/class/subtotal.class.php');
-		
+
 		$object = new Facture($db);
 		$object->fetch($element_id);
-		
+
 		if ($object->statut != Facture::STATUS_DRAFT) return 0;
-		
+
 		$res=1;
 		$nb_progress_updated = $nb_progress_not_updated = 0;
 		foreach ($object->lines as &$l)
@@ -130,7 +131,7 @@ function _updateProgress($element_id, $default_progress)
 			}
 			else $nb_progress_not_updated++;
 		}
-		
+
 		if ($nb_progress_updated > 0) setEventMessage($langs->trans('grapefruit_nb_progress_updated', $nb_progress_updated));
 		if ($nb_progress_not_updated > 0) setEventMessage($langs->trans('grapefruit_nb_progress_not_updated', $nb_progress_not_updated), 'warnings');
 	}
